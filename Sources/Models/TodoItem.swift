cbbb06f03f7a4d1049d42316969e06ecc6c0e30d
Evol Johnson <ejohnson@ngeosone.com>
2016-03-12 00:46:16 -0500

    Generated

---
+++ Sources/Models/TodoItem.swift
@@ -0,0 +1,106 @@
+/* 
+Copyright (c) 2016 NgeosOne LLC
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+   
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+ to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+
+Engineered using http://www.generatron.com/
+
+[GENERATRON]
+Generator :   System Templates
+Filename:     TodoItem.swift
+Description:  Class that holds the data for TodoItem
+Project:      TodoList
+Template: /Kitura/server/Entity.swift.vm
+ */
+
+class TodoItem  {
+    var completed : Bool!
+    var id : String!
+    var order : Int!
+    var title : String!
+    var url : String!
+    
+    
+    func toDictionary() -> Dictionary<String, JSONValue> {
+		var dict =  Dictionary<String, JSONValue>()
+		if(completed != nil){
+			dict["completed"] = completed
+		}
+		if(id != nil){
+			dict["id"] = id
+		}
+		if(order != nil){
+			dict["order"] = order
+		}
+		if(title != nil){
+			dict["title"] = title
+		}
+		if(url != nil){
+			dict["url"] = url
+		}
+		return dict        
+	}
+    
+    
+    func decode(jsonString : String) throws -> Void {
+        let decoder = JSONDecoder()
+        let payload = try decoder.decode(jsonString) as! JSONDictionaryType
+		if(payload["completed"] != nil){
+     		completed =  payload["completed"] as! Bool
+		}
+		if(payload["id"] != nil){
+     		id =  payload["id"] as! String
+		}
+		if(payload["order"] != nil){
+     		order =  payload["order"] as! Int
+		}
+		if(payload["title"] != nil){
+     		title =  payload["title"] as! String
+		}
+		if(payload["url"] != nil){
+     		url =  payload["url"] as! String
+		}
+    }
+    
+	func encode() throws -> String {
+        let encoder = JSONEncoder()
+        let payload = try encoder.encode(toDictionary())
+        return payload
+    }
+    
+    static func encodeList(elements : Array<TodoItem>) throws -> String {
+        var payload : Array<JSONValue> = [];
+        do {
+        elements.forEach { todoItem -> () in
+                payload.append(todoItem.toDictionary());
+           
+        }
+        let encoder = JSONEncoder()
+        let json = try encoder.encode(payload)
+        return json
+        }catch{
+            print(error)
+        }
+        return ""
+    }
+}
+
+/* 
+[STATS]
+It would take a person typing  @ 100.0 cpm, 
+approximately 17.56 minutes to type the 1756+ characters in this file.
+ */
+
+
---
+++ Sources/main.swift
@@ -0,0 +1,116 @@
+/* 
+Copyright (c) 2016 NgeosOne LLC
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+   
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+ to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+
+Engineered using http://www.generatron.com/
+
+[GENERATRON]
+Generator :   System Templates
+Filename:     main.swift
+Description:  Swift Server
+Project:      TodoList
+Template: /Kitura/server/main.swift.vmg
+ */
+
+import KituraSys
+import KituraNet
+import KituraRouter
+
+import LoggerAPI
+import HeliumLogger
+
+#if os(Linux)
+    import Glibc
+#endif
+
+import Foundation
+
+#if os(OSX)
+    import Mustache
+#endif
+
+// This route executes the echo middleware
+Router.sharedInstance.sharedInstance.use("/*", middleware: BasicAuthMiddleware())
+Router.sharedInstance.sharedInstance.use("/static/*", middleware: StaticFileServer())
+Router.sharedInstance.setTemplateEngine(MustacheTemplateEngine())
+
+Router.sharedInstance.get("/") { _, response, next in
+    defer {
+        next()
+    }
+    do {
+        var context: [String: Any] = [
+            "name": "Arthur",
+            "date": NSDate(),
+            "realDate": NSDate().dateByAddingTimeInterval(60*60*24*3),
+            "late": true
+        ]
+
+        // Let template format dates with `{{format(...)}}`
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateStyle = .MediumStyle
+        context["format"] = dateFormatter
+
+        try response.render("index", context: context).end()
+    } catch {
+        Log.error("Failed to render template \(error)")
+    }
+}
+
+// Handles any errors that get set
+Router.sharedInstance.error { request, response, next in
+  response.setHeader("Content-Type", value: "text/plain; charset=utf-8")
+    do {
+        try response.send("Caught the error: \(response.error!.localizedDescription)").end()
+    }
+    catch {}
+  next()
+}
+
+// A custom Not found handler
+Router.sharedInstance.all { request, response, next in
+    if  response.getStatusCode() == .NOT_FOUND  {
+        // Remove this wrapping if statement, if you want to handle requests to / as well
+        if  request.originalUrl != "/"  &&  request.originalUrl != ""  {
+            do {
+                try response.send("Route not found in Sample application!").end()
+            }
+            catch {}
+        }
+    }
+
+    next()
+}
+
+#let persistenceManager = PersistenceManagerMySQL();
+#let persistenceManager = PersistenceManagerSQLite();
+let persistenceManager = PersistenceManagerMemory();
+
+#if os(OSX) // Mustache implemented for OSX only yet
+
+#endif
+
+let server = HttpServer.listen(Config.sharedInstance.port,delegate: Router.sharedInstance)
+
+Server.run()
+
+/* 
+[STATS]
+It would take a person typing  @ 100.0 cpm, 
+approximately 22.77 minutes to type the 2277+ characters in this file.
+ */
+
+
