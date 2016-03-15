4f3bcf04728853e0ba4ce6f3075860ef22e0a79f
Evol Johnson <ejohnson@ngeosone.com>
2016-03-15 09:01:14 -0400

    Adding branch

--- TodoList/Sources/TodoList/Controllers/TodoItemController.swift
+++ TodoList/Sources/TodoList/Controllers/TodoItemController.swift
@@ -86,9 +86,12 @@ class TodoItemController  {
 	 TodoListRouter.sharedInstance.put("/api/todoItem"){ request, response, next in
 	    do {
 	     	if let body = request.body {
+	     		print("body")
             	if let json = body.asJson() {
+            	print(json)
             		let todoItem = TodoItem()
 	                try todoItem.deserialize(json);
+	                print("before update")
 		    		try self.pm.todoItemRepository.update(todoItem)
 		    		let json = try todoItem.encode()
 		    		response.status(HttpStatusCode.OK).sendJson(json)
